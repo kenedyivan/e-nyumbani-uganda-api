64881e242eae8b16615e34fa8705546bd58e6417
kenedyivan <kenedyakenaivan@gmail.com>
2017-10-26 16:30:57 +0300

    added required data to getAllListings method

--- app/Http/Controllers/MobileUser/ListingsController.php
+++ app/Http/Controllers/MobileUser/ListingsController.php
@@ -9,7 +9,24 @@ use App\Http\Controllers\Controller;
 class ListingsController extends Controller
 {
     function getAllListings(Request $request){
+
+    	$m_properties = array(); //All properties
+    	$p = array(); // A property
+
         $properties = Property::where('active', 1)->orderBy('id','DESC')->get();
-        return json_encode($properties);
+
+        foreach ($properties as $property) {
+        	
+        	$p['id'] = $property->id;
+        	$p['title'] = $property->title;
+        	$p['address'] = $property->address;
+        	$p['agent'] = $property->agent->username;
+        	$p['price'] = $property->price;
+        	$p['image'] = $property->image;
+
+        	array_push($m_properties, $p);
+        }
+        
+        return json_encode($m_properties);
     }
 }
