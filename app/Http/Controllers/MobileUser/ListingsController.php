b64df2e1529f2f59460eed92094d2a67b12ad57d
kenedyivan <kenedyakenaivan@gmail.com>
2017-10-26 15:28:13 +0300

    Added getAllListings method in ListingsController

--- app/Http/Controllers/MobileUser/ListingsController.php
+++ app/Http/Controllers/MobileUser/ListingsController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers\MobileUser;
 
+use App\Property;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ListingsController extends Controller
 {
-    //
+    function getAllListings(Request $request){
+        $properties = Property::where('active', 1)->orderBy('id','DESC')->get();
+        return json_encode($properties);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/test',function(){
     return "ok";
 });
+
+Route::get('/listings','MobileUser\ListingsController@getAllListings');
